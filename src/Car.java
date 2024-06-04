2b52d54d1e2842aa3d6c8730c9567717fced5c2f
AmirShoeibiOmrani <amirtahaomrany@gmail.com>
2024-06-04 17:04:44 -0500

    update...

---
+++ src/Car.java
@@ -0,0 +1,14 @@
+public class Car {
+
+    int wheel;
+    String color;
+    String owner;
+    public void printSum(int a, int b){
+        int sum = a + b ;
+        System.out.println(sum);
+    }
+    public int getSum(int age, int b , int c){
+        int sum = age + b+ c;
+        return sum;
+    }
+}
