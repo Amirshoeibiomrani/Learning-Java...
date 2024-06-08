f9d9366e27c774acf6005b887718244d61240589
AmirShoeibiOmrani <amirtahaomrany@gmail.com>
2024-06-08 17:47:51 -0500

    variables

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.text.MessageFormat;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -6,6 +8,30 @@ public class Main {
         System.out.println("Welcome\n to Java\n Tutorials!");
         System.out.println("Welcome \'to\' \"Java\"  Tutorials\t!");
         System.out.println("Welcome \\to\\ \fJava  Tutorials!");
+        String str= "Hello";
+        System.out.println(str);
 
+        //Declare variables
+        int num1;
+        int num2;
+        double num3;
+        float num4;
+        boolean boolVal;
+        char myChar;
+
+        //Assign values to variables
+        num1=2;
+        num2=3;
+        num3=4.33164634554555434545455d;
+        num4=5.1111f;
+        boolVal=true;
+        myChar='s';
+
+
+        //Show the values of the variables
+        System.out.println(MessageFormat.format("num1={0}",num1));
+        System.out.println(MessageFormat.format("num1={0}",num2));
+        System.out.println(MessageFormat.format("num1={0}",num3));
+        System.out.println(MessageFormat.format("num1={0}",num4));
     }
 }
